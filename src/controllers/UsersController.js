8c827c1ce787bbe6fe4b411ba40a4b8d7f09b66c
Anaildo Silva <157150090+tuhanzin@users.noreply.github.com>
2024-07-30 17:46:10 -0300

    Add files via upload

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,70 @@
+const { hash } = require("bcryptjs")
+
+const AppErrpr = require("../utils/appError")
+
+
+const sqliteConnection = require("../database/sqlite")
+const AppError = require("../utils/appError")
+
+class UsersController {
+  /*
+   * index - GET para listar varios registros
+  * show - get para exibir um registro expecifico
+  * create - post para criar um registro
+  * update - put para atualizar um registro
+  * delete - delete para remover um registro
+  */
+
+  async create(request, response) {
+    const { name, email, password} = request.body
+    
+    const database = await sqliteConnection()
+
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if(checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.")
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword])
+
+    return response.status(201).json()
+  }
+
+
+  async update(request, response) {
+    const {name, email} = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+    if(!user) {
+      throw new AppError("Usuário não encontrado.")
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este email já está em uso.")
+    }
+
+    user.name = name
+    user.email = email
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`
+      [user.name, user.email, new Date(), id]
+    )
+
+    return response.status(200).json()
+  }
+}
+
+module.exports = UsersController 
